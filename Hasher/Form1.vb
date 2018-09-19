f180c90dc0dd6c882fab7ca03c0b52bcb58c7bf9
Tom Parkison <trparky@toms-world.org>
2018-09-19 13:19:25 -0400

    Cleaned up some IF statements.

--- Hasher/Form1.vb
+++ Hasher/Form1.vb
@@ -668,18 +668,15 @@
 
         ' Figure out the new sorting order.
         Dim sort_order As SortOrder
-        If (m_SortingColumn2 Is Nothing) Then
+
+        If m_SortingColumn2 Is Nothing Then
             ' New column. Sort ascending.
             sort_order = SortOrder.Ascending
         Else
             ' See if this is the same column.
             If new_sorting_column.Equals(m_SortingColumn2) Then
                 ' Same column. Switch the sort order.
-                If m_SortingColumn2.Text.StartsWith("> ") Then
-                    sort_order = SortOrder.Descending
-                Else
-                    sort_order = SortOrder.Ascending
-                End If
+                sort_order = If(m_SortingColumn2.Text.StartsWith("> "), SortOrder.Descending, SortOrder.Ascending)
             Else
                 ' New column. Sort ascending.
                 sort_order = SortOrder.Ascending
