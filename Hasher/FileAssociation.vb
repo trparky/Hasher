072da9d897cfd95404ff2e21c67cdd255edbb7e2
Tom Parkison <trparky@toms-world.org>
2018-10-16 19:20:46 -0400

    Fixed file association code.

--- Hasher/FileAssociation.vb
+++ Hasher/FileAssociation.vb
@@ -7,30 +7,27 @@ Class FileAssociation
         LocalMachine
     End Enum
 
-    Public Shared Sub CreateAssociation(ByVal ProgID As String, ByVal extension As String, ByVal description As String, ByVal application As String, ByVal icon As String, ByVal Optional hive As KeyHiveSmall = KeyHiveSmall.CurrentUser)
+    Public Shared Sub CreateAssociation(ByVal extension As String, ByVal description As String, ByVal application As String, ByVal icon As String, ByVal Optional hive As KeyHiveSmall = KeyHiveSmall.CurrentUser)
         Dim selectedKey As RegistryKey = Nothing
 
         Select Case hive
             Case KeyHiveSmall.ClassesRoot
-                Registry.ClassesRoot.CreateSubKey(extension).SetValue("", ProgID)
-                selectedKey = Registry.ClassesRoot.CreateSubKey(ProgID)
+                selectedKey = Registry.ClassesRoot.CreateSubKey(extension)
             Case KeyHiveSmall.CurrentUser
-                Registry.CurrentUser.CreateSubKey("Software\Classes\" & extension).SetValue("", ProgID)
-                selectedKey = Registry.CurrentUser.CreateSubKey("Software\Classes\" & ProgID)
+                selectedKey = Registry.CurrentUser.CreateSubKey("Software\Classes\" & extension)
             Case KeyHiveSmall.LocalMachine
-                Registry.LocalMachine.CreateSubKey("Software\Classes\" & extension).SetValue("", ProgID)
-                selectedKey = Registry.LocalMachine.CreateSubKey("Software\Classes\" & ProgID)
+                selectedKey = Registry.LocalMachine.CreateSubKey("Software\Classes\" & extension)
         End Select
 
         If selectedKey IsNot Nothing Then
-            If description IsNot Nothing Then selectedKey.SetValue("", description)
+            If description IsNot Nothing Then selectedKey.SetValue(vbNullString, description)
 
             If icon IsNot Nothing Then
                 selectedKey.CreateSubKey("DefaultIcon").SetValue("", icon, RegistryValueKind.ExpandString)
-                selectedKey.CreateSubKey("Shell\Open").SetValue("icon", icon, RegistryValueKind.ExpandString)
+                selectedKey.CreateSubKey("Shell\Verify with Hasher").SetValue("icon", icon, RegistryValueKind.ExpandString)
             End If
 
-            If application IsNot Nothing Then selectedKey.CreateSubKey("Shell\Open\command").SetValue("", """" & application & """" & " --hashfile=""%1""", RegistryValueKind.ExpandString)
+            If application IsNot Nothing Then selectedKey.CreateSubKey("Shell\Verify with Hasher\command").SetValue("", """" & application & """" & " --hashfile=""%1""", RegistryValueKind.ExpandString)
 
             selectedKey.Flush()
             selectedKey.Close()
@@ -38,8 +35,7 @@ Class FileAssociation
     End Sub
 
     Public Shared Sub SelfCreateAssociation(ByVal extension As String, ByVal Optional hive As KeyHiveSmall = KeyHiveSmall.CurrentUser, ByVal Optional description As String = "")
-        Dim ProgID As String = Reflection.Assembly.GetExecutingAssembly().EntryPoint.DeclaringType.FullName
         Dim FileLocation As String = Reflection.Assembly.GetExecutingAssembly().Location
-        CreateAssociation(ProgID, extension, description, FileLocation, FileLocation & ",0", hive)
+        CreateAssociation(extension, description, FileLocation, FileLocation & ",0", hive)
     End Sub
 End Class
