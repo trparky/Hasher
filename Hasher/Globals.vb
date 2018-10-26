a08b48000dec2adfe9f9f68f37482ff5eaa310bd
Tom Parkison <trparky@toms-world.org>
2018-10-25 21:07:01 -0400

    Added the ability to kill any stray processes during the update process.

--- Hasher/Globals.vb
+++ Hasher/Globals.vb
@@ -10,6 +10,98 @@ Public Module Globals
         sha512
     End Enum
 
+    ''' <summary>Checks to see if a Process ID or PID exists on the system.</summary>
+    ''' <param name="PID">The PID of the process you are checking the existance of.</param>
+    ''' <param name="processObject">If the PID does exist, the function writes back to this argument in a ByRef way a Process Object that can be interacted with outside of this function.</param>
+    ''' <returns>Return a Boolean value. If the PID exists, it return a True value. If the PID doesn't exist, it returns a False value.</returns>
+    Private Function doesProcessIDExist(ByVal PID As Integer, ByRef processObject As Process) As Boolean
+        Try
+            processObject = Process.GetProcessById(PID)
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
+    Private Sub killProcess(processID As Integer, Optional boolLogToEventLog As Boolean = False)
+        Dim processObject As Process = Nothing
+
+        ' First we are going to check if the Process ID exists.
+        If doesProcessIDExist(processID, processObject) = True Then
+            Try
+                processObject.Kill() ' Yes, it does so let's kill it.
+            Catch ex As Exception
+                ' Wow, it seems that even with double-checking if a process exists by it's PID number things can still go wrong.
+                ' So this Try-Catch block is here to trap any possible errors when trying to kill a process by it's PID number.
+            End Try
+        End If
+
+        processObject = Nothing
+        Threading.Thread.Sleep(250) ' We're going to sleep to give the system some time to kill the process.
+
+        '' Now we are going to check again if the Process ID exists and if it does, we're going to attempt to kill it again.
+        If doesProcessIDExist(processID, processObject) = True Then
+            Try
+                processObject.Kill()
+            Catch ex As Exception
+                ' Wow, it seems that even with double-checking if a process exists by it's PID number things can still go wrong.
+                ' So this Try-Catch block is here to trap any possible errors when trying to kill a process by it's PID number.
+            End Try
+        End If
+
+        processObject = Nothing
+        Threading.Thread.Sleep(250) ' We're going to sleep (again) to give the system some time to kill the process.
+    End Sub
+
+    Private Function getProcessExecutablePath(processID As Integer) As String
+        Dim memoryBuffer = New Text.StringBuilder(1024)
+        Dim processHandle As IntPtr = NativeMethod.NativeMethods.OpenProcess(NativeMethod.ProcessAccessFlags.PROCESS_QUERY_LIMITED_INFORMATION, False, processID)
+
+        If processHandle <> IntPtr.Zero Then
+            Try
+                Dim memoryBufferSize As Integer = memoryBuffer.Capacity
+
+                If NativeMethod.NativeMethods.QueryFullProcessImageName(processHandle, 0, memoryBuffer, memoryBufferSize) Then
+                    Return memoryBuffer.ToString()
+                End If
+            Finally
+                NativeMethod.NativeMethods.CloseHandle(processHandle)
+            End Try
+        End If
+
+        Return Nothing
+    End Function
+
+    Public Sub searchForProcessAndKillIt(strFileName As String, boolFullFilePathPassed As Boolean)
+        Dim processExecutablePath As String
+        Dim processExecutablePathFileInfo As IO.FileInfo
+
+        For Each process As Process In Process.GetProcesses()
+            processExecutablePath = getProcessExecutablePath(process.Id)
+
+            If processExecutablePath IsNot Nothing Then
+                Try
+                    processExecutablePathFileInfo = New IO.FileInfo(processExecutablePath)
+
+                    If boolFullFilePathPassed = True Then
+                        If strFileName.Equals(processExecutablePathFileInfo.FullName, StringComparison.OrdinalIgnoreCase) = True Then
+                            killProcess(process.Id, True)
+                        End If
+                    ElseIf boolFullFilePathPassed = False Then
+                        If strFileName.Equals(processExecutablePathFileInfo.Name, StringComparison.OrdinalIgnoreCase) = True Then
+                            killProcess(process.Id, True)
+                        End If
+                    End If
+
+                    processExecutablePathFileInfo = Nothing
+                Catch ex As ArgumentException
+                End Try
+            End If
+
+            processExecutablePath = Nothing
+        Next
+    End Sub
+
     Public Function areWeAnAdministrator() As Boolean
         Try
             Dim principal As WindowsPrincipal = New WindowsPrincipal(WindowsIdentity.GetCurrent())
--- Hasher/NativeMethod.vb
+++ Hasher/NativeMethod.vb
@@ -8,5 +8,37 @@ Namespace NativeMethod
         <DllImport("user32.dll", EntryPoint:="SendMessageW")>
         Public Shared Function SendMessage(ByVal hWnd As IntPtr, ByVal Msg As UInteger, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As Integer
         End Function
+
+        <DllImport("kernel32.dll")>
+        Friend Shared Function OpenProcess(dwDesiredAccess As ProcessAccessFlags, bInheritHandle As Boolean, dwProcessId As Integer) As IntPtr
+        End Function
+
+        <DllImport("kernel32.dll", SetLastError:=True)>
+        Friend Shared Function CloseHandle(hHandle As IntPtr) As Boolean
+        End Function
+
+        <DllImport("kernel32.dll", CharSet:=CharSet.Unicode)>
+        Friend Shared Function QueryFullProcessImageName(hprocess As IntPtr, dwFlags As Integer, lpExeName As Text.StringBuilder, ByRef size As Integer) As Boolean
+        End Function
     End Class
+
+    Module APIs
+        <Flags>
+        Public Enum ProcessAccessFlags As UInteger
+            PROCESS_QUERY_LIMITED_INFORMATION = &H1000
+            All = &H1F0FFF
+            Terminate = &H1
+            CreateThread = &H2
+            VirtualMemoryOperation = &H8
+            VirtualMemoryRead = &H10
+            VirtualMemoryWrite = &H20
+            DuplicateHandle = &H40
+            CreateProcess = &H80
+            SetQuota = &H100
+            SetInformation = &H200
+            QueryInformation = &H400
+            QueryLimitedInformation = &H1000
+            Synchronize = &H100000
+        End Enum
+    End Module
 End Namespace
