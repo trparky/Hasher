49ae8b87d49ad709ace4563b879ebd1aae9fc6e1
Tom Parkison <trparky@toms-world.org>
2018-12-19 22:26:29 -0500

    Added Properties to the myListViewItem Class.

--- Hasher/myListViewItem.vb
+++ Hasher/myListViewItem.vb
@@ -1,10 +1,11 @@
 ﻿' This class extends the ListViewItem so that I can add more properties to it for my purposes.
 Public Class myListViewItem
     Inherits ListViewItem
-    Public fileSize As Long
-    Public hash, fileName As String
-    Public color As Color
-    Public boolFileExists As Boolean
+    Public Property fileSize As Long
+    Public Property hash As String
+    Public Property fileName As String
+    Public Property color As Color
+    Public Property boolFileExists As Boolean
 
     Public Sub New(strInput As String)
         Me.Text = strInput
