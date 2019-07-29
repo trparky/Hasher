cb84e393f8d4fa369cbb428c1e431e49d45bbf2e
Tom Parkison <trparky@toms-world.org>
2019-07-29 18:58:57 -0400

    Fixed a NullReferenceException crash that could occur when pressing the Delete button the keyboard after selecting files in the file listbox using the mouse.

--- Hasher/listViewSorter.vb
+++ Hasher/listViewSorter.vb
@@ -23,23 +23,27 @@ Class ListViewComparer
         strFirstString = If(lvFirstListView.SubItems.Count <= intColumnNumber, "", lvFirstListView.SubItems(intColumnNumber).Text)
         strSecondString = If(lvSecondListView.SubItems.Count <= intColumnNumber, "", lvSecondListView.SubItems(intColumnNumber).Text)
 
-        ' Compare them.
-        If (lvFirstListView.ListView.Name = "verifyHashesListFiles" Or lvFirstListView.ListView.Name = "listFiles") And intColumnNumber = 1 Then
-            long1 = DirectCast(lvFirstListView, myListViewItem).fileSize
-            long2 = DirectCast(lvSecondListView, myListViewItem).fileSize
+        If lvFirstListView.ListView IsNot Nothing Then
+            ' Compare them.
+            If (lvFirstListView.ListView.Name = "verifyHashesListFiles" Or lvFirstListView.ListView.Name = "listFiles") And intColumnNumber = 1 Then
+                long1 = DirectCast(lvFirstListView, myListViewItem).fileSize
+                long2 = DirectCast(lvSecondListView, myListViewItem).fileSize
 
-            Return If(soSortOrder = SortOrder.Ascending, long1.CompareTo(long2), long2.CompareTo(long1))
-        ElseIf (lvFirstListView.ListView.Name = "verifyHashesListFiles" Or lvFirstListView.ListView.Name = "listFiles") And intColumnNumber = 3 Then
-            timespan1 = DirectCast(lvFirstListView, myListViewItem).computeTime
-            timespan2 = DirectCast(lvSecondListView, myListViewItem).computeTime
-
-            Return If(soSortOrder = SortOrder.Ascending, timespan1.CompareTo(timespan2), timespan2.CompareTo(timespan1))
-        Else
-            If Long.TryParse(strFirstString, long1) And Long.TryParse(strSecondString, long2) Then
                 Return If(soSortOrder = SortOrder.Ascending, long1.CompareTo(long2), long2.CompareTo(long1))
+            ElseIf (lvFirstListView.ListView.Name = "verifyHashesListFiles" Or lvFirstListView.ListView.Name = "listFiles") And intColumnNumber = 3 Then
+                timespan1 = DirectCast(lvFirstListView, myListViewItem).computeTime
+                timespan2 = DirectCast(lvSecondListView, myListViewItem).computeTime
+
+                Return If(soSortOrder = SortOrder.Ascending, timespan1.CompareTo(timespan2), timespan2.CompareTo(timespan1))
             Else
-                Return If(soSortOrder = SortOrder.Ascending, String.Compare(strFirstString, strSecondString), String.Compare(strSecondString, strFirstString))
+                If Long.TryParse(strFirstString, long1) And Long.TryParse(strSecondString, long2) Then
+                    Return If(soSortOrder = SortOrder.Ascending, long1.CompareTo(long2), long2.CompareTo(long1))
+                Else
+                    Return If(soSortOrder = SortOrder.Ascending, String.Compare(strFirstString, strSecondString), String.Compare(strSecondString, strFirstString))
+                End If
             End If
+        Else
+            Return 0
         End If
     End Function
 End Class
