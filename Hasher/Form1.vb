dcddcfda1c4c7e0328e13a69114763071d3a5e79
Tom Parkison <trparky@toms-world.org>
2019-09-08 21:21:26 -0400

    Added code to reset the two ULong Byte counting variables, ulongAllBytes and ulongAllReadBytes.

--- Hasher/Form1.vb
+++ Hasher/Form1.vb
@@ -283,6 +283,8 @@ Public Class Form1
 
                                                      hashIndividualFilesAllFilesProgressBar.Value = 0
                                                      hashIndividualFilesAllFilesProgressBar.Visible = False
+                                                     ulongAllBytes = 0
+                                                     ulongAllReadBytes = 0
                                                  End Try
                                              End Sub) With {
             .Priority = Threading.ThreadPriority.Highest,
@@ -919,6 +921,8 @@ Public Class Form1
 
                                                      verifyIndividualFilesAllFilesProgressBar.Value = 0
                                                      verifyIndividualFilesAllFilesProgressBar.Visible = False
+                                                     ulongAllBytes = 0
+                                                     ulongAllReadBytes = 0
                                                  End Try
                                              End Sub) With {
             .Priority = Threading.ThreadPriority.Highest,
