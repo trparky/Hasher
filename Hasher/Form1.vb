ed8e68f3d822cf2fcf9750679dff28f3d380d42f
Tom Parkison <trparky@toms-world.org>
2020-10-03 11:27:18 -0400

    Fixed a MAJOR bug that broke just about everything!!!

--- Hasher/Form1.vb
+++ Hasher/Form1.vb
@@ -865,8 +865,8 @@ Public Class Form1
         lblProcessingFileVerify.Text = Nothing
         lblCompareFileAgainstKnownHashType.Text = Nothing
         lblCompareFilesAllFilesStatus.Text = Nothing
-        lblIndividualFilesStatusProcessingFile = Nothing
-        lblHashIndividualFilesTotalStatus = Nothing
+        lblIndividualFilesStatusProcessingFile.Text = Nothing
+        lblHashIndividualFilesTotalStatus.Text = Nothing
         chkRecurrsiveDirectorySearch.Checked = My.Settings.boolRecurrsiveDirectorySearch
         chkSSL.Checked = My.Settings.boolSSL
         chkSortByFileSizeAfterLoadingHashFile.Checked = My.Settings.boolSortByFileSizeAfterLoadingHashFile
