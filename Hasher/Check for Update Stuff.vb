0755ba1008683d603ed8c23b012194b38e07b3c8
Tom Parkison <trparky@toms-world.org>
2020-11-24 11:54:27 -0500

    Removed the requirement for the CanIWriteToTheCurrentDirectory() and renamed the CheckByFolderACLs() function to CheckFolderPermissionsByACLs().

--- Hasher/Check for Update Stuff.vb
+++ Hasher/Check for Update Stuff.vb
@@ -216,11 +216,7 @@ Class Check_for_Update_Stuff
         End Try
     End Function
 
-    Private Shared Function CanIWriteToTheCurrentDirectory() As Boolean
-        Return CheckByFolderACLs(New FileInfo(Application.ExecutablePath).DirectoryName)
-    End Function
-
-    Private Shared Function CheckByFolderACLs(folderPath As String) As Boolean
+    Private Shared Function CheckFolderPermissionsByACLs(folderPath As String) As Boolean
         Try
             Dim directoryACLs As DirectorySecurity = Directory.GetAccessControl(folderPath)
             Dim directoryAccessRights As FileSystemAccessRule
@@ -362,7 +358,7 @@ Class Check_for_Update_Stuff
             .FileName = newExecutableName,
             .Arguments = "-update"
         }
-        If Not CanIWriteToTheCurrentDirectory() Then startInfo.Verb = "runas"
+        If Not CheckFolderPermissionsByACLs(New FileInfo(Application.ExecutablePath).DirectoryName) Then startInfo.Verb = "runas"
         Process.Start(startInfo)
 
         Process.GetCurrentProcess.Kill()
