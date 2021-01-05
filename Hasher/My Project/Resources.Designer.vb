43b3c93a2ee6e3de05496ef53ad1fe6e80c69633
Tom Parkison <trparky@toms-world.org>
2021-01-05 13:22:47 -0500

    Removed the informationIcon resource, it's no longer used.

--- Hasher/My Project/Resources.Designer.vb
+++ Hasher/My Project/Resources.Designer.vb
@@ -103,16 +103,6 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property informationIcon() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("informationIcon", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
         Friend ReadOnly Property UAC() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("UAC", resourceCulture)
