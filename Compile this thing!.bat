2e7d513437364c8ae139f6c3be143eb79d896b94
Tom Parkison <trparky@toms-world.org>
2021-01-17 09:58:49 -0500

    Removed "Compile this thing!.bat" from GIT.

--- Compile this thing!.bat
+++
@@ -1,5 +0,0 @@
-MSBuild.exe "Hasher.sln" /noconsolelogger /t:Rebuild /p:Configuration=Debug
-MSBuild.exe "Hasher.sln" /noconsolelogger /t:Rebuild /p:Configuration=Release
-copy "Hasher\bin\Release\Hasher.exe" "%OneDrive%\Utilities\Hasher.exe"
-cd Hasher\bin\Release
-call "Package This Program!.bat"
