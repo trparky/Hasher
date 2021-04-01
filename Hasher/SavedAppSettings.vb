95f6b6ae0cf623839da70f0af5c15dbd69a88077
Tom Parkison <trparky@toms-world.org>
2021-04-01 14:15:45 -0400

    Rewrote much of the program settings export and import code based upon what was used in one of my past programs, namely System Restore Point Creator.

--- Hasher/SavedAppSettings.vb
+++ Hasher/SavedAppSettings.vb
@@ -1,140 +1,87 @@
 ﻿Imports System.Web.Script.Serialization
 
+Public Class ExportedSettings
+    Public strName As String, value As Object
+    Public type As String
+End Class
+
 Public Module SavedAppSettingsModule
     Public Sub SaveApplicationSettingsToFile(strFileName As String)
+        Dim exportedSettingsArray As New List(Of ExportedSettings)
+        Dim exportedSettingsObject As ExportedSettings
+        Dim settingType As Type
+        Dim point As Point, size As Size
+
+        For Each settingProperty As Configuration.SettingsPropertyValue In My.Settings.PropertyValues
+            If settingProperty.PropertyValue IsNot Nothing Then
+                settingType = settingProperty.PropertyValue.GetType
+                exportedSettingsObject = New ExportedSettings With {.strName = settingProperty.Name, .type = settingProperty.PropertyValue.GetType.ToString}
+
+                If settingType = GetType(Point) Then
+                    point = DirectCast(settingProperty.PropertyValue, Point)
+                    exportedSettingsObject.value = point.X & "|" & point.Y
+                    point = Nothing
+                ElseIf settingType = GetType(Color) Then
+                    exportedSettingsObject.value = DirectCast(settingProperty.PropertyValue, Color).ToArgb
+                ElseIf settingType = GetType(Size) Then
+                    size = DirectCast(settingProperty.PropertyValue, Size)
+                    exportedSettingsObject.value = size.Height & "|" & size.Width
+                    size = Nothing
+                Else
+                    exportedSettingsObject.value = settingProperty.PropertyValue
+                End If
+
+                exportedSettingsArray.Add(exportedSettingsObject)
+                exportedSettingsObject = Nothing
+            End If
+        Next
+
         Using streamWriter As New IO.StreamWriter(strFileName)
             Dim json As JavaScriptSerializer = New JavaScriptSerializer()
-            streamWriter.Write(json.Serialize(ExportApplicationSettings()))
+            streamWriter.Write(json.Serialize(exportedSettingsArray))
         End Using
     End Sub
 
     Public Sub LoadApplicationSettingsFromFile(strFileName As String)
-        Dim SavedAppSettingsInstance As New SavedAppSettings
+        Dim exportedSettingsArray As New List(Of ExportedSettings)
+        Dim ExportedSettingsObject As ExportedSettings
+        Dim boolResult As Boolean, byteResult As Byte, intResult As Integer, longResult As Long, shortResult As Short, splitArray As String()
 
         Using streamReader As New IO.StreamReader(strFileName)
             Dim json As JavaScriptSerializer = New JavaScriptSerializer()
-            SavedAppSettingsInstance = json.Deserialize(Of SavedAppSettings)(streamReader.ReadToEnd.Trim)
+            exportedSettingsArray = json.Deserialize(Of List(Of ExportedSettings))(streamReader.ReadToEnd.Trim)
         End Using
 
-        ImportApplicationSettings(SavedAppSettingsInstance)
+        For Each settingProperty As Configuration.SettingsPropertyValue In My.Settings.PropertyValues
+            Dim IEnumerableObject As IEnumerable(Of ExportedSettings) = exportedSettingsArray.Where(Function(item As ExportedSettings) item.strName.Equals(settingProperty.Name, StringComparison.OrdinalIgnoreCase))
+
+            If IEnumerableObject.Count <> 0 AndAlso IEnumerableObject(0) IsNot Nothing Then
+                ExportedSettingsObject = IEnumerableObject(0)
+
+                If ExportedSettingsObject.type.CaseInsensitiveContains("Drawing.Color") Then
+                    My.Settings(ExportedSettingsObject.strName) = Color.FromArgb(ExportedSettingsObject.value)
+                ElseIf ExportedSettingsObject.type.CaseInsensitiveContains("Drawing.Point") Then
+                    splitArray = ExportedSettingsObject.value.split("|")
+                    My.Settings(ExportedSettingsObject.strName) = New Point() With {.X = splitArray(0), .Y = splitArray(1)}
+                    splitArray = Nothing
+                ElseIf ExportedSettingsObject.type.CaseInsensitiveContains("Drawing.Size") Then
+                    splitArray = ExportedSettingsObject.value.split("|")
+                    My.Settings(ExportedSettingsObject.strName) = New Size() With {.Height = splitArray(0), .Width = splitArray(1)}
+                    splitArray = Nothing
+                ElseIf ExportedSettingsObject.type.CaseInsensitiveContains("Boolean") Then
+                    If Boolean.TryParse(ExportedSettingsObject.value, boolResult) Then My.Settings(ExportedSettingsObject.strName) = boolResult
+                ElseIf ExportedSettingsObject.type.CaseInsensitiveContains("Byte") Then
+                    If Byte.TryParse(ExportedSettingsObject.value, byteResult) Then My.Settings(ExportedSettingsObject.strName) = byteResult
+                ElseIf ExportedSettingsObject.type.CaseInsensitiveContains("Int16") Then
+                    If Short.TryParse(ExportedSettingsObject.value, shortResult) Then My.Settings(ExportedSettingsObject.strName) = shortResult
+                ElseIf ExportedSettingsObject.type.CaseInsensitiveContains("Int32") Then
+                    If Integer.TryParse(ExportedSettingsObject.value, intResult) Then My.Settings(ExportedSettingsObject.strName) = intResult
+                ElseIf ExportedSettingsObject.type.CaseInsensitiveContains("Int64") Then
+                    If Long.TryParse(ExportedSettingsObject.value, longResult) Then My.Settings(ExportedSettingsObject.strName) = longResult
+                Else
+                    My.Settings(ExportedSettingsObject.strName) = ExportedSettingsObject.value
+                End If
+            End If
+        Next
     End Sub
-
-    Private Function ExportApplicationSettings() As SavedAppSettings
-        Dim SavedAppSettings As New SavedAppSettings With {
-            .boolAutoAddExtension = My.Settings.boolAutoAddExtension,
-            .boolCheckForUpdates = My.Settings.boolCheckForUpdates,
-            .boolComputeHashesOnCompareFilesTabEvenWithDifferentFileSizes = My.Settings.boolComputeHashesOnCompareFilesTabEvenWithDifferentFileSizes,
-            .boolDisplayHashesInUpperCase = My.Settings.boolDisplayHashesInUpperCase,
-            .boolDisplayValidChecksumString = My.Settings.boolDisplayValidChecksumString,
-            .boolIncludeEntryCountInFileNameHeader = My.Settings.boolIncludeEntryCountInFileNameHeader,
-            .boolOpenInExplorer = My.Settings.boolOpenInExplorer,
-            .boolRecurrsiveDirectorySearch = My.Settings.boolRecurrsiveDirectorySearch,
-            .boolSaveChecksumFilesWithRelativePaths = My.Settings.boolSaveChecksumFilesWithRelativePaths,
-            .boolShowFileProgressInFileList = My.Settings.boolShowFileProgressInFileList,
-            .boolShowPercentageInWindowTitleBar = My.Settings.boolShowPercentageInWindowTitleBar,
-            .boolSortByFileSizeAfterLoadingHashFile = My.Settings.boolSortByFileSizeAfterLoadingHashFile,
-            .boolSortFileListingAfterAddingFilesToHash = My.Settings.boolSortFileListingAfterAddingFilesToHash,
-            .boolUseCommasInNumbers = My.Settings.boolUseCommasInNumbers,
-            .boolUseMilliseconds = My.Settings.boolUseMilliseconds,
-            .boolWindowMaximized = My.Settings.boolWindowMaximized,
-            .defaultHash = My.Settings.defaultHash,
-            .fileNotFoundColor = My.Settings.fileNotFoundColor.ToArgb,
-            .hashIndividualFilesChecksumColumnSize = My.Settings.hashIndividualFilesChecksumColumnSize,
-            .hashIndividualFilesComputeTimeColumnSize = My.Settings.hashIndividualFilesComputeTimeColumnSize,
-            .hashIndividualFilesFileNameColumnSize = My.Settings.hashIndividualFilesFileNameColumnSize,
-            .hashIndividualFilesFileSizeColumnSize = My.Settings.hashIndividualFilesFileSizeColumnSize,
-            .newHashChecksumColumnSize = My.Settings.newHashChecksumColumnSize,
-            .notValidColor = My.Settings.notValidColor.ToArgb,
-            .roundFileSizes = My.Settings.roundFileSizes,
-            .roundPercentages = My.Settings.roundPercentages,
-            .shortBufferSize = My.Settings.shortBufferSize,
-            .taskPriority = My.Settings.taskPriority,
-            .validColor = My.Settings.validColor.ToArgb,
-            .verifyHashComputeTimeColumnSize = My.Settings.verifyHashComputeTimeColumnSize,
-            .verifyHashFileNameColumnSize = My.Settings.verifyHashFileNameColumnSize,
-            .verifyHashFileResults = My.Settings.verifyHashFileResults,
-            .verifyHashFileSizeColumnSize = My.Settings.verifyHashFileSizeColumnSize,
-            .windowLocation = My.Settings.windowLocation,
-            .windowSize = My.Settings.windowSize
-        }
-        Return SavedAppSettings
-    End Function
-
-    Private Sub ImportApplicationSettings(SavedAppSettings As SavedAppSettings)
-        With My.Settings
-            .boolAutoAddExtension = SavedAppSettings.boolAutoAddExtension
-            .boolCheckForUpdates = SavedAppSettings.boolCheckForUpdates
-            .boolComputeHashesOnCompareFilesTabEvenWithDifferentFileSizes = SavedAppSettings.boolComputeHashesOnCompareFilesTabEvenWithDifferentFileSizes
-            .boolDisplayHashesInUpperCase = SavedAppSettings.boolDisplayHashesInUpperCase
-            .boolDisplayValidChecksumString = SavedAppSettings.boolDisplayValidChecksumString
-            .boolIncludeEntryCountInFileNameHeader = SavedAppSettings.boolIncludeEntryCountInFileNameHeader
-            .boolOpenInExplorer = SavedAppSettings.boolOpenInExplorer
-            .boolRecurrsiveDirectorySearch = SavedAppSettings.boolRecurrsiveDirectorySearch
-            .boolSaveChecksumFilesWithRelativePaths = SavedAppSettings.boolSaveChecksumFilesWithRelativePaths
-            .boolShowFileProgressInFileList = SavedAppSettings.boolShowFileProgressInFileList
-            .boolShowPercentageInWindowTitleBar = SavedAppSettings.boolShowPercentageInWindowTitleBar
-            .boolSortByFileSizeAfterLoadingHashFile = SavedAppSettings.boolSortByFileSizeAfterLoadingHashFile
-            .boolSortFileListingAfterAddingFilesToHash = SavedAppSettings.boolSortFileListingAfterAddingFilesToHash
-            .boolUseCommasInNumbers = SavedAppSettings.boolUseCommasInNumbers
-            .boolUseMilliseconds = SavedAppSettings.boolUseMilliseconds
-            .boolWindowMaximized = SavedAppSettings.boolWindowMaximized
-            .defaultHash = SavedAppSettings.defaultHash
-            .fileNotFoundColor = Color.FromArgb(SavedAppSettings.fileNotFoundColor)
-            .hashIndividualFilesChecksumColumnSize = SavedAppSettings.hashIndividualFilesChecksumColumnSize
-            .hashIndividualFilesComputeTimeColumnSize = SavedAppSettings.hashIndividualFilesComputeTimeColumnSize
-            .hashIndividualFilesFileNameColumnSize = SavedAppSettings.hashIndividualFilesFileNameColumnSize
-            .hashIndividualFilesFileSizeColumnSize = SavedAppSettings.hashIndividualFilesFileSizeColumnSize
-            .newHashChecksumColumnSize = SavedAppSettings.newHashChecksumColumnSize
-            .notValidColor = Color.FromArgb(SavedAppSettings.notValidColor)
-            .roundFileSizes = SavedAppSettings.roundFileSizes
-            .roundPercentages = SavedAppSettings.roundPercentages
-            .shortBufferSize = SavedAppSettings.shortBufferSize
-            .taskPriority = SavedAppSettings.taskPriority
-            .validColor = Color.FromArgb(SavedAppSettings.validColor)
-            .verifyHashComputeTimeColumnSize = SavedAppSettings.verifyHashComputeTimeColumnSize
-            .verifyHashFileNameColumnSize = SavedAppSettings.verifyHashFileNameColumnSize
-            .verifyHashFileResults = SavedAppSettings.verifyHashFileResults
-            .verifyHashFileSizeColumnSize = SavedAppSettings.verifyHashFileSizeColumnSize
-            .windowLocation = SavedAppSettings.windowLocation
-            .windowSize = SavedAppSettings.windowSize
-        End With
-    End Sub
-
-    Private Structure SavedAppSettings
-        Public boolAutoAddExtension As Boolean
-        Public boolCheckForUpdates As Boolean
-        Public boolComputeHashesOnCompareFilesTabEvenWithDifferentFileSizes As Boolean
-        Public boolDisplayHashesInUpperCase As Boolean
-        Public boolDisplayValidChecksumString As Boolean
-        Public boolIncludeEntryCountInFileNameHeader As Boolean
-        Public boolOpenInExplorer As Boolean
-        Public boolRecurrsiveDirectorySearch As Boolean
-        Public boolSaveChecksumFilesWithRelativePaths As Boolean
-        Public boolShowFileProgressInFileList As Boolean
-        Public boolShowPercentageInWindowTitleBar As Boolean
-        Public boolSortByFileSizeAfterLoadingHashFile As Boolean
-        Public boolSortFileListingAfterAddingFilesToHash As Boolean
-        Public boolUseCommasInNumbers As Boolean
-        Public boolUseMilliseconds As Boolean
-        Public boolWindowMaximized As Boolean
-        Public defaultHash As Byte
-        Public fileNotFoundColor As Integer
-        Public hashIndividualFilesChecksumColumnSize As Short
-        Public hashIndividualFilesComputeTimeColumnSize As Short
-        Public hashIndividualFilesFileNameColumnSize As Short
-        Public hashIndividualFilesFileSizeColumnSize As Short
-        Public newHashChecksumColumnSize As Short
-        Public notValidColor As Integer
-        Public roundFileSizes As Byte
-        Public roundPercentages As Byte
-        Public shortBufferSize As Short
-        Public taskPriority As Byte
-        Public validColor As Integer
-        Public verifyHashComputeTimeColumnSize As Short
-        Public verifyHashFileNameColumnSize As Short
-        Public verifyHashFileResults As Short
-        Public verifyHashFileSizeColumnSize As Short
-        Public windowLocation As Point
-        Public windowSize As Size
-    End Structure
 End Module
