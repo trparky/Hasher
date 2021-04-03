de8d7762fd0bd900b92289294b835ae3d16d317f
Tom Parkison <trparky@toms-world.org>
2021-04-02 21:30:33 -0400

    Rewrote the way the data is stored in a settings JSON file.

--- Hasher/SavedAppSettings.vb
+++ Hasher/SavedAppSettings.vb
@@ -1,38 +1,31 @@
 ﻿Imports System.Web.Script.Serialization
 
-Public Class ExportedSettings
-    Public strName As String, value As Object
-    Public type As String
-End Class
-
 Public Module SavedAppSettingsModule
     Public Sub SaveApplicationSettingsToFile(strFileName As String)
-        Dim exportedSettingsArray As New List(Of ExportedSettings)
-        Dim exportedSettingsObject As ExportedSettings
+        Dim exportedSettingsArray As New Dictionary(Of String, Object)
         Dim settingType As Type
         Dim point As Point, size As Size
+        Dim value As Object
 
         For Each settingProperty As Configuration.SettingsPropertyValue In My.Settings.PropertyValues
             If settingProperty.PropertyValue IsNot Nothing Then
                 settingType = settingProperty.PropertyValue.GetType
-                exportedSettingsObject = New ExportedSettings With {.strName = settingProperty.Name, .type = settingProperty.PropertyValue.GetType.ToString}
 
                 If settingType = GetType(Point) Then
                     point = DirectCast(settingProperty.PropertyValue, Point)
-                    exportedSettingsObject.value = point.X & "|" & point.Y
+                    value = point.X & "|" & point.Y
                     point = Nothing
                 ElseIf settingType = GetType(Color) Then
-                    exportedSettingsObject.value = DirectCast(settingProperty.PropertyValue, Color).ToArgb
+                    value = DirectCast(settingProperty.PropertyValue, Color).ToArgb
                 ElseIf settingType = GetType(Size) Then
                     size = DirectCast(settingProperty.PropertyValue, Size)
-                    exportedSettingsObject.value = size.Height & "|" & size.Width
+                    value = size.Height & "|" & size.Width
                     size = Nothing
                 Else
-                    exportedSettingsObject.value = settingProperty.PropertyValue
+                    value = settingProperty.PropertyValue
                 End If
 
-                exportedSettingsArray.Add(exportedSettingsObject)
-                exportedSettingsObject = Nothing
+                exportedSettingsArray.Add(settingProperty.Name, value)
             End If
         Next
 
@@ -43,41 +36,42 @@ Public Module SavedAppSettingsModule
     End Sub
 
     Public Sub LoadApplicationSettingsFromFile(strFileName As String)
-        Dim exportedSettingsArray As New List(Of ExportedSettings)
-        Dim exportedSettingsObject As ExportedSettings
-        Dim boolResult As Boolean, byteResult As Byte, intResult As Integer, longResult As Long, shortResult As Short, splitArray As String()
+        Dim exportedSettingsArray As New Dictionary(Of String, Object)
+        Dim boolResult As Boolean, byteResult As Byte, intResult As Integer, longResult As Long, settingType As Type, shortResult As Short, splitArray As String(), value As Object
 
         Using streamReader As New IO.StreamReader(strFileName)
             Dim json As JavaScriptSerializer = New JavaScriptSerializer()
-            exportedSettingsArray = json.Deserialize(Of List(Of ExportedSettings))(streamReader.ReadToEnd.Trim)
+            exportedSettingsArray = json.Deserialize(Of Dictionary(Of String, Object))(streamReader.ReadToEnd.Trim)
         End Using
 
         For Each settingProperty As Configuration.SettingsPropertyValue In My.Settings.PropertyValues
-            exportedSettingsObject = exportedSettingsArray.ToList.Find(Function(item As ExportedSettings) item.strName.Equals(settingProperty.Name, StringComparison.OrdinalIgnoreCase))
+            If exportedSettingsArray.ContainsKey(settingProperty.Name) Then
+                Debug.WriteLine("importing data for " & settingProperty.Name)
+                settingType = settingProperty.PropertyValue.GetType
+                value = exportedSettingsArray.Item(settingProperty.Name)
 
-            If exportedSettingsObject IsNot Nothing Then
-                If exportedSettingsObject.type.CaseInsensitiveContains("Drawing.Color") Then
-                    My.Settings(exportedSettingsObject.strName) = Color.FromArgb(exportedSettingsObject.value)
-                ElseIf exportedSettingsObject.type.CaseInsensitiveContains("Drawing.Point") Then
-                    splitArray = exportedSettingsObject.value.split("|")
-                    My.Settings(exportedSettingsObject.strName) = New Point() With {.X = splitArray(0), .Y = splitArray(1)}
+                If settingType = GetType(Color) Then
+                    My.Settings(settingProperty.Name) = Color.FromArgb(value)
+                ElseIf settingType = GetType(Point) Then
+                    splitArray = value.split("|")
+                    My.Settings(settingProperty.Name) = New Point() With {.X = splitArray(0), .Y = splitArray(1)}
                     splitArray = Nothing
-                ElseIf exportedSettingsObject.type.CaseInsensitiveContains("Drawing.Size") Then
-                    splitArray = exportedSettingsObject.value.split("|")
-                    My.Settings(exportedSettingsObject.strName) = New Size() With {.Height = splitArray(0), .Width = splitArray(1)}
+                ElseIf settingType = GetType(Size) Then
+                    splitArray = value.split("|")
+                    My.Settings(settingProperty.Name) = New Size() With {.Height = splitArray(0), .Width = splitArray(1)}
                     splitArray = Nothing
-                ElseIf exportedSettingsObject.type.CaseInsensitiveContains("Boolean") Then
-                    If Boolean.TryParse(exportedSettingsObject.value, boolResult) Then My.Settings(exportedSettingsObject.strName) = boolResult
-                ElseIf exportedSettingsObject.type.CaseInsensitiveContains("Byte") Then
-                    If Byte.TryParse(exportedSettingsObject.value, byteResult) Then My.Settings(exportedSettingsObject.strName) = byteResult
-                ElseIf exportedSettingsObject.type.CaseInsensitiveContains("Int16") Or exportedSettingsObject.type.CaseInsensitiveContains("Short") Then
-                    If Short.TryParse(exportedSettingsObject.value, shortResult) Then My.Settings(exportedSettingsObject.strName) = shortResult
-                ElseIf exportedSettingsObject.type.CaseInsensitiveContains("Int32") Or exportedSettingsObject.type.CaseInsensitiveContains("Integer") Then
-                    If Integer.TryParse(exportedSettingsObject.value, intResult) Then My.Settings(exportedSettingsObject.strName) = intResult
-                ElseIf exportedSettingsObject.type.CaseInsensitiveContains("Int64") Or exportedSettingsObject.type.CaseInsensitiveContains("Long") Then
-                    If Long.TryParse(exportedSettingsObject.value, longResult) Then My.Settings(exportedSettingsObject.strName) = longResult
+                ElseIf settingType = GetType(Boolean) Then
+                    If Boolean.TryParse(value, boolResult) Then My.Settings(settingProperty.Name) = boolResult
+                ElseIf settingType = GetType(Byte) Then
+                    If Byte.TryParse(value, byteResult) Then My.Settings(settingProperty.Name) = byteResult
+                ElseIf settingType = GetType(Short) Then
+                    If Short.TryParse(value, shortResult) Then My.Settings(settingProperty.Name) = shortResult
+                ElseIf settingType = GetType(Integer) Then
+                    If Integer.TryParse(value, intResult) Then My.Settings(settingProperty.Name) = intResult
+                ElseIf settingType = GetType(Long) Then
+                    If Long.TryParse(value, longResult) Then My.Settings(settingProperty.Name) = longResult
                 Else
-                    My.Settings(exportedSettingsObject.strName) = exportedSettingsObject.value
+                    My.Settings(settingProperty.Name) = value
                 End If
             End If
         Next
