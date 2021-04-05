ec4e4294d7d436b2273b1c23aa16e2cb77cb3450
Tom Parkison <trparky@toms-world.org>
2021-04-05 14:06:33 -0400

    Renamed the value variable to rawValue.

--- Hasher/SavedAppSettings.vb
+++ Hasher/SavedAppSettings.vb
@@ -5,7 +5,7 @@ Public Module SavedAppSettingsModule
         Dim exportedSettingsArray As New Dictionary(Of String, Object)
         Dim settingType As Type
         Dim point As Point, size As Size
-        Dim value As Object
+        Dim rawValue As Object
 
         For Each settingProperty As Configuration.SettingsPropertyValue In My.Settings.PropertyValues
             If settingProperty.PropertyValue IsNot Nothing Then
@@ -13,19 +13,19 @@ Public Module SavedAppSettingsModule
 
                 If settingType = GetType(Point) Then
                     point = DirectCast(settingProperty.PropertyValue, Point)
-                    value = point.X & "|" & point.Y
+                    rawValue = point.X & "|" & point.Y
                     point = Nothing
                 ElseIf settingType = GetType(Color) Then
-                    value = DirectCast(settingProperty.PropertyValue, Color).ToArgb
+                    rawValue = DirectCast(settingProperty.PropertyValue, Color).ToArgb
                 ElseIf settingType = GetType(Size) Then
                     size = DirectCast(settingProperty.PropertyValue, Size)
-                    value = size.Height & "|" & size.Width
+                    rawValue = size.Height & "|" & size.Width
                     size = Nothing
                 Else
-                    value = settingProperty.PropertyValue
+                    rawValue = settingProperty.PropertyValue
                 End If
 
-                exportedSettingsArray.Add(settingProperty.Name.Trim.ToLower, value)
+                exportedSettingsArray.Add(settingProperty.Name.Trim.ToLower, rawValue)
             End If
         Next
 
@@ -38,7 +38,7 @@ Public Module SavedAppSettingsModule
     Public Sub LoadApplicationSettingsFromFile(strFileName As String)
         Dim exportedSettingsArray As New Dictionary(Of String, Object)
         Dim boolResult As Boolean, byteResult As Byte, intResult As Integer, longResult As Long, settingType As Type, shortResult As Short, splitArray As String()
-        Dim value As Object = Nothing
+        Dim rawValue As Object = Nothing
 
         Using streamReader As New IO.StreamReader(strFileName)
             Dim json As JavaScriptSerializer = New JavaScriptSerializer()
@@ -46,31 +46,31 @@ Public Module SavedAppSettingsModule
         End Using
 
         For Each settingProperty As Configuration.SettingsPropertyValue In My.Settings.PropertyValues
-            If exportedSettingsArray.TryGetValue(settingProperty.Name.Trim.ToLower, value) Then
+            If exportedSettingsArray.TryGetValue(settingProperty.Name.Trim.ToLower, rawValue) Then
                 settingType = settingProperty.PropertyValue.GetType
 
                 If settingType = GetType(Color) Then
-                    If Integer.TryParse(value, intResult) Then My.Settings(settingProperty.Name) = Color.FromArgb(intResult)
+                    If Integer.TryParse(rawValue, intResult) Then My.Settings(settingProperty.Name) = Color.FromArgb(intResult)
                 ElseIf settingType = GetType(Point) Then
-                    splitArray = value.split("|")
+                    splitArray = rawValue.split("|")
                     My.Settings(settingProperty.Name) = New Point() With {.X = splitArray(0), .Y = splitArray(1)}
                     splitArray = Nothing
                 ElseIf settingType = GetType(Size) Then
-                    splitArray = value.split("|")
+                    splitArray = rawValue.split("|")
                     My.Settings(settingProperty.Name) = New Size() With {.Height = splitArray(0), .Width = splitArray(1)}
                     splitArray = Nothing
                 ElseIf settingType = GetType(Boolean) Then
-                    If Boolean.TryParse(value, boolResult) Then My.Settings(settingProperty.Name) = boolResult
+                    If Boolean.TryParse(rawValue, boolResult) Then My.Settings(settingProperty.Name) = boolResult
                 ElseIf settingType = GetType(Byte) Then
-                    If Byte.TryParse(value, byteResult) Then My.Settings(settingProperty.Name) = byteResult
+                    If Byte.TryParse(rawValue, byteResult) Then My.Settings(settingProperty.Name) = byteResult
                 ElseIf settingType = GetType(Short) Then
-                    If Short.TryParse(value, shortResult) Then My.Settings(settingProperty.Name) = shortResult
+                    If Short.TryParse(rawValue, shortResult) Then My.Settings(settingProperty.Name) = shortResult
                 ElseIf settingType = GetType(Integer) Then
-                    If Integer.TryParse(value, intResult) Then My.Settings(settingProperty.Name) = intResult
+                    If Integer.TryParse(rawValue, intResult) Then My.Settings(settingProperty.Name) = intResult
                 ElseIf settingType = GetType(Long) Then
-                    If Long.TryParse(value, longResult) Then My.Settings(settingProperty.Name) = longResult
+                    If Long.TryParse(rawValue, longResult) Then My.Settings(settingProperty.Name) = longResult
                 Else
-                    My.Settings(settingProperty.Name) = value
+                    My.Settings(settingProperty.Name) = rawValue
                 End If
             End If
         Next
