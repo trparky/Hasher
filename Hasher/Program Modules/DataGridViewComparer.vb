c864b8e041a40af9b17feb39a9c3070bd2164b19
Thomas Parkison <trparky@toms-world.org>
2024-05-16 12:03:59 -0400

    Remove the ability sorting ability from the DataGridViewComparer() Class for anything other than the File Size column since that's all we use the sorting class for.

--- Hasher/Program Modules/DataGridViewComparer.vb
+++ Hasher/Program Modules/DataGridViewComparer.vb
@@ -10,22 +10,12 @@
     End Sub
 
     Public Function Compare(row1 As DataGridViewRow, row2 As DataGridViewRow) As Integer Implements IComparer(Of DataGridViewRow).Compare
-        Dim strFirstString, strSecondString As String
         Dim fileSize1, fileSize2 As Long
 
-        ' Get the cell values.
-        strFirstString = If(row1.Cells.Count <= intColumnNumber, "", row1.Cells(intColumnNumber).Value?.ToString())
-        strSecondString = If(row2.Cells.Count <= intColumnNumber, "", row2.Cells(intColumnNumber).Value?.ToString())
-
-        ' Compare them.
-        If intColumnNumber = 1 Then
-            If TypeOf row1 Is MyDataGridViewRow AndAlso TypeOf row2 Is MyDataGridViewRow Then
-                fileSize1 = DirectCast(row1, MyDataGridViewRow).FileSize
-                fileSize2 = DirectCast(row2, MyDataGridViewRow).FileSize
-                Return If(soSortOrder = SortOrder.Ascending, fileSize1.CompareTo(fileSize2), fileSize2.CompareTo(fileSize1))
-            End If
-        Else
-            Return If(soSortOrder = SortOrder.Ascending, String.Compare(strFirstString, strSecondString), String.Compare(strSecondString, strFirstString))
+        If intColumnNumber = 1 AndAlso TypeOf row1 Is MyDataGridViewRow AndAlso TypeOf row2 Is MyDataGridViewRow Then
+            fileSize1 = DirectCast(row1, MyDataGridViewRow).FileSize
+            fileSize2 = DirectCast(row2, MyDataGridViewRow).FileSize
+            Return If(soSortOrder = SortOrder.Ascending, fileSize1.CompareTo(fileSize2), fileSize2.CompareTo(fileSize1))
         End If
 
         Return 0
