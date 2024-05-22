b14d33adc860ceb54df0980784c6357b949f589c
Thomas Parkison <trparky@toms-world.org>
2024-05-21 21:45:35 -0400

    FIxed order of sorting.

--- Hasher/Program Modules/DataGridViewComparer.vb
+++ Hasher/Program Modules/DataGridViewComparer.vb
@@ -14,17 +14,17 @@
             Dim fileSize1 As Long = row1.FileSize
             Dim fileSize2 As Long = row2.FileSize
 
-            Return If(soSortOrder = Global.System.Windows.Forms.SortOrder.Ascending, fileSize1.CompareTo(fileSize2), fileSize2.CompareTo(fileSize1))
+            Return If(soSortOrder = SortOrder.Descending, fileSize1.CompareTo(fileSize2), fileSize2.CompareTo(fileSize1))
         ElseIf intColumnNumber = 3 Then
             Dim timeSpan1 As TimeSpan = row1.ComputeTime
             Dim timeSpan2 As TimeSpan = row2.ComputeTime
 
-            Return If(soSortOrder = Global.System.Windows.Forms.SortOrder.Ascending, timeSpan1.CompareTo(timeSpan2), timeSpan2.CompareTo(timeSpan1))
+            Return If(soSortOrder = SortOrder.Descending, timeSpan1.CompareTo(timeSpan2), timeSpan2.CompareTo(timeSpan1))
         Else
             Dim strFirstString As String = If(row1.Cells.Count <= intColumnNumber, "", row1.Cells(intColumnNumber).Value?.ToString())
             Dim strSecondString As String = If(row2.Cells.Count <= intColumnNumber, "", row2.Cells(intColumnNumber).Value?.ToString())
 
-            Return If(soSortOrder = SortOrder.Ascending, String.Compare(strFirstString, strSecondString), String.Compare(strSecondString, strFirstString))
+            Return If(soSortOrder = SortOrder.Descending, String.Compare(strFirstString, strSecondString), String.Compare(strSecondString, strFirstString))
         End If
 
         Return 0
