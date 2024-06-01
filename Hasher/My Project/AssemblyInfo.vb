4100913c53eef4472e70b808e3a15cf3d07946be
Thomas Parkison <trparky@toms-world.org>
2024-06-01 19:54:21 -0400

    Updated the version.

--- Hasher/My Project/AssemblyInfo.vb
+++ Hasher/My Project/AssemblyInfo.vb
@@ -31,4 +31,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("8.0.2.16")>
+<Assembly: AssemblyFileVersion("8.0.3.17")>
